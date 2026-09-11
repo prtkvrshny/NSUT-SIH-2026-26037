5e0c24fab39318432580bfd515125ac45cdefcd4
prtkvrshny <prateekvarshney2007@gmail.com>
2026-09-11 12:43:36 +0530

    Delete src/SIH_26037_TOOLBOX_INTEGRATED/SIH_26037 directory

--- src/SIH_26037_TOOLBOX_INTEGRATED/SIH_26037/sensors/initTrackList.m
+++
@@ -1,7 +0,0 @@
-function tracks = initTrackList()
-% initTrackList  Empty track struct array with the exact field set
-% expected by sensors/trackObstacles.m. Call once before the first
-% tracking update in any scenario.
-tracks = struct('id',{},'x',{},'y',{},'vx',{},'vy',{},'type',{}, ...
-                 'confirmed',{},'age',{},'missedFrames',{});
-end
